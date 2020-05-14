c413f7c4d471ee7e1cde50d9ddb34a5122d40f68
GSK-TEST <64266892+GSK-TEST@users.noreply.github.com>
2020-05-14 17:57:02 +0300

    тест к обнове 1.0

---
+++ test/base_test/nt/js/ot.js
@@ -0,0 +1,428 @@
+var res = "4344344133214233322";
+function check_me() {
+  var count = 0;
+  with (document.test) {
+    if (!Q1[0].checked && !Q1[1].checked && !Q1[2].checked && !Q1[3].checked) {
+      count += 1;
+    }
+    if (!Q2[0].checked && !Q2[1].checked && !Q2[2].checked && !Q2[3].checked) {
+      count += 1;
+    }
+    if (!Q3[0].checked && !Q3[1].checked && !Q3[2].checked && !Q3[3].checked) {
+      count += 1;
+    }
+    if (!Q4[0].checked && !Q4[1].checked && !Q4[2].checked && !Q4[3].checked) {
+      count += 1;
+    }
+    if (!Q5[0].checked && !Q5[1].checked && !Q5[2].checked && !Q5[3].checked) {
+      count += 1;
+    }
+    if (!Q6[0].checked && !Q6[1].checked && !Q6[2].checked && !Q6[3].checked) {
+      count += 1;
+    }
+    if (!Q7[0].checked && !Q7[1].checked && !Q7[2].checked && !Q7[3].checked) {
+      count += 1;
+    }
+    if (!Q8[0].checked && !Q8[1].checked && !Q8[2].checked && !Q8[3].checked) {
+      count += 1;
+    }
+    if (!Q9[0].checked && !Q9[1].checked && !Q9[2].checked && !Q9[3].checked) {
+      count += 1;
+    }
+    if (
+      !Q10[0].checked &&
+      !Q10[1].checked &&
+      !Q10[2].checked &&
+      !Q10[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q11[0].checked &&
+      !Q11[1].checked &&
+      !Q11[2].checked &&
+      !Q11[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q12[0].checked &&
+      !Q12[1].checked &&
+      !Q12[2].checked &&
+      !Q12[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q13[0].checked &&
+      !Q13[1].checked &&
+      !Q13[2].checked &&
+      !Q13[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q14[0].checked &&
+      !Q14[1].checked &&
+      !Q14[2].checked &&
+      !Q14[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q15[0].checked &&
+      !Q15[1].checked &&
+      !Q15[2].checked &&
+      !Q15[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q16[0].checked &&
+      !Q16[1].checked &&
+      !Q16[2].checked &&
+      !Q16[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q17[0].checked &&
+      !Q17[1].checked &&
+      !Q17[2].checked &&
+      !Q17[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q18[0].checked &&
+      !Q18[1].checked &&
+      !Q18[2].checked &&
+      !Q18[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q19[0].checked &&
+      !Q19[1].checked &&
+      !Q19[2].checked &&
+      !Q19[3].checked
+    ) {
+      count += 1;
+    }
+    if (
+      !Q20[0].checked &&
+      !Q20[1].checked &&
+      !Q20[2].checked &&
+      !Q20[3].checked
+    ) {
+      count += 1;
+    }
+    if (count > 0) alert("Вы выполнили не все задания. Проверьте себя!");
+    else answer();
+  }
+}
+
+function control(
+  k,
+  f1,
+  f2,
+  f3,
+  f4,
+  f5,
+  f6,
+  f7,
+  f8,
+  f9,
+  f10,
+  f11,
+  f12,
+  f13,
+  f14,
+  f15,
+  f16,
+  f17,
+  f18,
+  f19,
+  f20
+) {
+  if (k == 1 && f1.checked) return true;
+  if (k == 2 && f2.checked) return true;
+  if (k == 3 && f3.checked) return true;
+  if (k == 4 && f4.checked) return true;
+  if (k == 5 && f5.checked) return true;
+  if (k == 6 && f6.checked) return true;
+  if (k == 7 && f7.checked) return true;
+  if (k == 8 && f8.checked) return true;
+  if (k == 9 && f9.checked) return true;
+  if (k == 10 && f10.checked) return true;
+  if (k == 11 && f11.checked) return true;
+  if (k == 12 && f12.checked) return true;
+  if (k == 13 && f13.checked) return true;
+  if (k == 14 && f14.checked) return true;
+  if (k == 15 && f15.checked) return true;
+  if (k == 16 && f16.checked) return true;
+  if (k == 17 && f17.checked) return true;
+  if (k == 18 && f18.checked) return true;
+  if (k == 19 && f19.checked) return true;
+  if (k == 20 && f20.checked) return true;
+  return false;
+}
+
+function answer() {
+  answ = "";
+  with (document) {
+    answ += control(res.charAt(0), test.Q1[0], test.Q1[1], test.Q1[2], test.Q1[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(1), test.Q2[0], test.Q2[1], test.Q2[2], test.Q2[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(2), test.Q3[0], test.Q3[1], test.Q3[2], test.Q3[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(3), test.Q4[0], test.Q4[1], test.Q4[2], test.Q4[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(4), test.Q5[0], test.Q5[1], test.Q5[2], test.Q5[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(5), test.Q6[0], test.Q6[1], test.Q6[2], test.Q6[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(6), test.Q7[0], test.Q7[1], test.Q7[2], test.Q7[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(7), test.Q8[0], test.Q8[1], test.Q8[2], test.Q8[3])
+      ? "1"
+      : "0";
+    answ += control(res.charAt(8), test.Q9[0], test.Q9[1], test.Q9[2], test.Q9[3])
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(9),
+      test.Q10[0],
+      test.Q10[1],
+      test.Q10[2],
+      test.Q10[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(10),
+      test.Q11[0],
+      test.Q11[1],
+      test.Q11[2],
+      test.Q11[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(11),
+      test.Q12[0],
+      test.Q12[1],
+      test.Q12[2],
+      test.Q12[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(12),
+      test.Q13[0],
+      test.Q13[1],
+      test.Q13[2],
+      test.Q13[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(13),
+      test.Q14[0],
+      test.Q14[1],
+      test.Q14[2],
+      test.Q14[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(14),
+      test.Q15[0],
+      test.Q15[1],
+      test.Q15[2],
+      test.Q15[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(15),
+      test.Q16[0],
+      test.Q16[1],
+      test.Q16[2],
+      test.Q16[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(16),
+      test.Q17[0],
+      test.Q17[1],
+      test.Q17[2],
+      test.Q17[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(17),
+      test.Q18[0],
+      test.Q18[1],
+      test.Q18[2],
+      test.Q18[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(18),
+      test.Q19[0],
+      test.Q19[1],
+      test.Q19[2],
+      test.Q19[3]
+    )
+      ? "1"
+      : "0";
+    answ += control(
+      res.charAt(19),
+      test.Q20[0],
+      test.Q20[1],
+      test.Q20[2],
+      test.Q20[3]
+    )
+      ? "1"
+      : "0";
+
+    showResult();
+  }
+}
+
+function showResult() {
+  var nok = 0;
+  var i, s;
+
+  for (i = 0; i < answ.length; i++) {
+    nok += answ.charAt(i) == "1" ? 1 : 0;
+  }
+  if (nok == 20) s = "ОТЛИЧНО";
+  if (nok < 20) s = "ХОРОШО";
+  if (nok < 15) s = "УДОВЛЕТВОРИТЕЛЬНО";
+  if (nok < 10) s = "НЕУДОВЛЕТВОРИТЕЛЬНО";
+  document.test.s1.value =
+    "Количество правильных ответов " +
+    nok +
+    ". Ваша оценка " +
+    s 
+
+  with (document.test) {
+    if (answ.charAt(0) == "1") {
+      T1.value = " + ";
+    } else {
+      T1.value = " - ";
+    }
+    if (answ.charAt(1) == "1") {
+      T2.value = " + ";
+    } else {
+      T2.value = " - ";
+    }
+    if (answ.charAt(2) == "1") {
+      T3.value = " + ";
+    } else {
+      T3.value = " - ";
+    }
+    if (answ.charAt(3) == "1") {
+      T4.value = " + ";
+    } else {
+      T4.value = " - ";
+    }
+    if (answ.charAt(4) == "1") {
+      T5.value = " + ";
+    } else {
+      T5.value = " - ";
+    }
+    if (answ.charAt(5) == "1") {
+      T6.value = " + ";
+    } else {
+      T6.value = " - ";
+    }
+    if (answ.charAt(6) == "1") {
+      T7.value = " + ";
+    } else {
+      T7.value = " - ";
+    }
+    if (answ.charAt(7) == "1") {
+      T8.value = " + ";
+    } else {
+      T8.value = " - ";
+    }
+    if (answ.charAt(8) == "1") {
+      T9.value = " + ";
+    } else {
+      T9.value = " - ";
+    }
+    if (answ.charAt(9) == "1") {
+      T10.value = " + ";
+    } else {
+      T10.value = " - ";
+    }
+    if (answ.charAt(10) == "1") {
+      T11.value = " + ";
+    } else {
+      T11.value = " - ";
+    }
+    if (answ.charAt(11) == "1") {
+      T12.value = " + ";
+    } else {
+      T12.value = " - ";
+    }
+    if (answ.charAt(12) == "1") {
+      T13.value = " + ";
+    } else {
+      T13.value = " - ";
+    }
+    if (answ.charAt(13) == "1") {
+      T14.value = " + ";
+    } else {
+      T14.value = " - ";
+    }
+    if (answ.charAt(14) == "1") {
+      T15.value = " + ";
+    } else {
+      T15.value = " - ";
+    }
+    if (answ.charAt(15) == "1") {
+      T16.value = " + ";
+    } else {
+      T16.value = " - ";
+    }
+    if (answ.charAt(16) == "1") {
+      T17.value = " + ";
+    } else {
+      T17.value = " - ";
+    }
+    if (answ.charAt(17) == "1") {
+      T18.value = " + ";
+    } else {
+      T18.value = " - ";
+    }
+    if (answ.charAt(18) == "1") {
+      T19.value = " + ";
+    } else {
+      T19.value = " - ";
+    }
+    if (answ.charAt(19) == "1") {
+      T20.value = " + ";
+    } else {
+      T20.value = " - ";
+    }
+  }
+}
+function showhide(obj) {
+  if (obj == "none") return "inline";
+  else return "none";
+}
